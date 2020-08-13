64f366e7904b7a9830d5af7a404a83c480ef5284
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-08-13 11:57:31 -0500

    Pragmatic navigation

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -29,14 +29,20 @@
           />
         </td>
         <td>
-          <b>#{{ a.rank }} </b>
+          <b>#{{ a.rank }}</b>
         </td>
         <td>
-          {{ a.name }}
+          <router-link
+            class="hover:undelined text-green-600"
+            :to="{
+              name: 'coin-detail',
+              params: { id: a.id }
+            }"
+            >{{ a.name }}</router-link
+          >
+          <small class="ml-1 text-gray-500">{{ a.symbol }}</small>
         </td>
-        <td>
-          {{ a.priceUsd | dollar }}
-        </td>
+        <td>{{ a.priceUsd | dollar }}</td>
         <td>{{ a.marketCapUsd | dollar }}</td>
         <td
           :class="
@@ -47,19 +53,30 @@
         >
           {{ a.changePercent24Hr | percent }}
         </td>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <px-button @custon-click="goToCoin(a.id)">
+            <span>Detalle</span>
+          </px-button>
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import PxButton from "@/components/PxButton"
 export default {
   name: "PxAssetsTable",
+  components: { PxButton },
   props: {
     assets: {
       type: Array,
       default: () => []
+    }
+  },
+  methods: {
+    goToCoin(id) {
+      this.$router.push({ name: "coin-detail", params: { id } })
     }
   }
 }
